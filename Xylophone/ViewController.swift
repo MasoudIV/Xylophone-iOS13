f356462d2ae16b36382385eda2b71d451434d13e
MasoudFarmani <info.masoudfarmani@gmail.com>
2023-09-30 17:01:24 +0330

    Project Finished!

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,13 +7,38 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    var player : AVAudioPlayer?
+    
+    
+    @IBAction func keyTouched(_ sender: UIButton) {
+        let nameList = ["C", "D", "E", "F", "G", "A", "B"]
+        let soundName = nameList[sender.tag]
+        playSound(soundName)
+        UIView.animate(withDuration: 0.1) {
+            sender.alpha = 0.5
+        }
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+            UIView.animate(withDuration: 0.1) {
+                 sender.alpha = 1.0
+           }
+        }
     }
-
+    
+    func playSound(_ name: String){
+        guard let url = Bundle.main.url(forResource: name, withExtension: "wav") else {
+            return
+        }
+        do {
+            player = try AVAudioPlayer(contentsOf: url)
+            player?.play()
+        }catch let error {
+            print(error.localizedDescription)
+        }
+    }
 
     
 
